df374577dfbc67d2195ecae0d00052a7a5920df3
Emma Litwa-Vulcu <80431903+reiniiriarios@users.noreply.github.com>
2024-03-30 13:40:52 -0700

    feat: sort books

--- frontend/pages/books/books.svelte
+++ frontend/pages/books/books.svelte
@@ -5,6 +5,7 @@
   import { Book } from "@data/book";
 
   let allBooks: Book[] = [];
+  let sortedBooks: Book[] = [];
 
   function readBooks() {
     window.electronAPI.readAllBooks();
@@ -16,7 +17,53 @@
 
   window.electronAPI.receiveAllBooks((books: Book[]) => {
     allBooks = books;
+    sortedBooks = books;
   });
+
+  let currentSort: string = "";
+
+  function sortFilter(i: string, sort: (books: Book[]) => Book[]) {
+    currentSort = i;
+    sortedBooks = sort(structuredClone(allBooks));
+  }
+
+  const sortFilters = {
+    author: {
+      name: "Author",
+      sort: (books: Book[]): Book[] => {
+        books.sort((x, y) => {
+          let xA = x.authors[0].name.split(" ").pop();
+          let yA = y.authors[0].name.split(" ").pop();
+          return (xA ?? "").localeCompare(yA ?? "");
+        });
+        return books;
+      },
+    },
+    title: {
+      name: "Title",
+      sort: (books: Book[]): Book[] => {
+        books.sort((x, y) => {
+          let xT = x.title.replace(/^(?:A|An|The) /i, "");
+          let yT = y.title.replace(/^(?:A|An|The) /i, "");
+          return xT.localeCompare(yT);
+        });
+        return books;
+      },
+    },
+    datePublished: {
+      name: "Publish Date",
+      sort: (books: Book[]): Book[] => {
+        books.sort((x, y) => {
+          let xD = new Date(x.datePublished ?? "").getTime();
+          let yD = new Date(y.datePublished ?? "").getTime();
+          if (xD < yD) return -1;
+          if (xD > yD) return 1;
+          return 0;
+        });
+        return books;
+      },
+    },
+  };
 </script>
 
 <div class="pageNav">
@@ -26,8 +73,24 @@
     <SearchBook />
   </div>
 </div>
+<div class="listFilter">
+  <div class="filter">
+    <span>Sort:</span>
+    {#each Object.entries(sortFilters) as [i, s]}
+      <button on:click={() => sortFilter(i, s.sort)} class:selected={currentSort === i}>{s.name}</button>
+    {/each}
+  </div>
+  <div class="filter">
+    <span>Filter:</span>
+    <button data-filter="fiction">Fiction</button>
+    <button data-filter="nonfiction">Non-Fiction</button>
+    <button data-filter="scifi">Science Fiction</button>
+    <button data-filter="fantasy">Fantasy</button>
+    <button data-filter="nonfiction">Non-Fiction</button>
+  </div>
+</div>
 <div class="bookList">
-  {#each allBooks as book}
+  {#each sortedBooks as book}
     <div class="book">
       {#if book.hasImage}
         <a href={`#/book/${book.authorDir}/${book.filename}`} class="book__inner book__inner--image">
@@ -48,6 +111,37 @@
 <style lang="scss">
   @import "../../style/variables";
 
+  $filterHeight: 3rem;
+
+  .listFilter {
+    height: $filterHeight;
+    padding: 0.5rem 1rem 1.25rem;
+    display: flex;
+    gap: 2rem;
+    align-items: center;
+    justify-content: left;
+
+    .filter {
+      display: flex;
+      align-items: center;
+      justify-content: left;
+      gap: 0.25rem;
+
+      button {
+        background-color: $bgColorLight;
+        color: $fgColor;
+        padding: 0.25rem 0.5rem;
+        border: 0;
+        border-radius: 0.25rem;
+        cursor: pointer;
+
+        &.selected {
+          background-color: $bgColorLighter;
+        }
+      }
+    }
+  }
+
   .bookList {
     display: grid;
     grid-template-columns: 1fr 1fr 1fr 1fr;
@@ -55,7 +149,7 @@
     padding: 0.5rem 1rem 1.25rem;
     overflow-y: auto;
     width: 100%;
-    height: calc(100vh - $pageNavHeight);
+    height: calc(100vh - $pageNavHeight - $filterHeight);
     scrollbar-width: thin;
     scrollbar-color: $bgColorLightest transparent;
   }
