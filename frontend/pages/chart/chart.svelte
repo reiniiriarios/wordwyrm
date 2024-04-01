37b7b33107f9d4f4aece594d1f800a2e6842bf3e
Emma Litwa-Vulcu <80431903+reiniiriarios@users.noreply.github.com>
2024-04-01 14:16:44 -0700

    feat: chart start year

--- frontend/pages/chart/chart.svelte
+++ frontend/pages/chart/chart.svelte
@@ -4,63 +4,65 @@
   import { Book } from "@data/book";
   import { onMount } from "svelte";
 
-  const startDate = new Date(2020, 1, 1);
+  let startYear = 2020;
+  const startDate = new Date(startYear, 1, 1);
   const endDate = new Date();
+  let allBooks: Book[] = [];
 
-  let years: Record<number, number> = ((): Record<number, number> => {
+  function initYears(): Record<number, number> {
     let ys: Record<string, number> = {};
     let startYear = startDate.getFullYear();
     let endYear = endDate.getFullYear();
-    for (let y = startYear; y <= endYear; y++) ys[y] = 0;
+    for (let y = startYear; y <= endYear; y++) {
+      ys[y] = 0;
+    }
     return ys;
-  })();
+  }
 
   const mAbbr = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-  let months: Record<string, { display: string; count: number }> = ((): Record<
-    string,
-    { display: string; count: number }
-  > => {
+  function initMonths(): Record<string, { display: string; count: number }> {
     let ms: Record<string, { display: string; count: number }> = {};
     let startYear = startDate.getFullYear();
     let endYear = endDate.getFullYear();
     for (let y = startYear; y <= endYear; y++) {
       for (let m = 0; m < 12; m++) {
-        ms[y + "-" + m.toString().padStart(2, "0")] = {
+        let ym = y + "-" + m.toString().padStart(2, "0");
+        ms[ym] = {
           display: mAbbr[m] + " " + y,
           count: 0,
         };
       }
     }
     return ms;
-  })();
+  }
 
-  let days: Record<string, number> = ((): Record<string, number> => {
+  function initDays(): Record<string, number> {
     let ds: Record<string, number> = {};
     let currentDate = startDate;
     while (currentDate <= endDate) {
-      ds[
+      let ymd =
         currentDate.getFullYear() +
-          "-" +
-          currentDate.getMonth().toString().padStart(2, "0") +
-          "-" +
-          currentDate.getDate().toString().padStart(2, "0")
-      ] = 0;
+        "-" +
+        currentDate.getMonth().toString().padStart(2, "0") +
+        "-" +
+        currentDate.getDate().toString().padStart(2, "0");
+      ds[ymd] = 0;
       let newDate = new Date(currentDate);
       newDate.setDate(currentDate.getDate() + 1);
       currentDate = newDate;
     }
     return ds;
-  })();
+  }
+
+  let years: Record<number, number> = initYears();
+  let months: Record<string, { display: string; count: number }> = initMonths();
+  let days: Record<string, number> = initDays();
 
   let chartCanvas: HTMLCanvasElement;
   let chart: Chart;
 
-  onMount(() => {
-    window.electronAPI.readAllBooks();
-  });
-
-  window.electronAPI.receiveAllBooks((books: Book[]) => {
-    books.forEach((b) => {
+  function parseBooks() {
+    allBooks.forEach((b) => {
       if (b.dateRead) {
         let t = new Date(b.dateRead);
         let y = t.getFullYear();
@@ -71,6 +73,24 @@
         if (typeof days[d] !== "undefined") days[d]++;
       }
     });
+  }
+
+  function setStartYear() {
+    startDate.setFullYear(startYear);
+    years = initYears();
+    months = initMonths();
+    days = initDays();
+    parseBooks();
+    setView();
+  }
+
+  onMount(() => {
+    window.electronAPI.readAllBooks();
+  });
+
+  window.electronAPI.receiveAllBooks((books: Book[]) => {
+    allBooks = books;
+    parseBooks();
 
     // Wait for the canvas element to appear, THEN run chart code.
     new Promise((resolve) => {
@@ -113,7 +133,7 @@
 
   let view: "year" | "month" | "day" = "month";
 
-  function setView(v: "year" | "month" | "day") {
+  function setView(v: "year" | "month" | "day" = view) {
     view = v;
     switch (v) {
       case "day":
@@ -136,6 +156,8 @@
 <div class="pageNav">
   <h2 class="pageNav__header">Trend</h2>
   <div class="pageNav__actions">
+    <div>Start:</div>
+    <input type="number" bind:value={startYear} on:change={setStartYear} min="1900" max={new Date().getFullYear()} />
     <button class="btn btn--round" class:selected={view === "day"} on:click={() => setView("day")}>Days</button>
     <button class="btn btn--round" class:selected={view === "month"} on:click={() => setView("month")}>Months</button>
     <button class="btn btn--round" class:selected={view === "year"} on:click={() => setView("year")}>Years</button>
